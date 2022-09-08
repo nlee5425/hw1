a887463ffdcf4f26bcbf4290d7072509175f8a49
NathanLee <nlee5425@usc.edu>
2022-09-08 08:57:11 +0000

    Code changes for problem 1

--- split.cpp
+++ split.cpp
@@ -17,24 +17,44 @@ the function below should be the only one in this file.
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-  if ( in == nullptr) {
+  if ( in == nullptr ) {
     return;
   } 
+  else if( in->value <= 0 ) {
+    split( in->next, odds, evens);
+  }
   else if ( in->value % 2 == 0 ) {
     evens = in;
+    //odds = nullptr;
     split( in->next, odds, evens->next);
   }
   else {
-    odds = in;   
+    odds = in; 
+    //evens = nullptr;  
     split( in->next, odds->next, evens);
   }
-  if ( evens->value % 2 == 1) {
+  // if (evens->value <=0) {
+  //   evens = nullptr;
+  // }
+  // else if (odds->value <= 0) {
+  //   odds = nullptr;
+  // }
+  if ( in->next == nullptr ) {
+    if ( odds == nullptr) {
+      return;
+    } 
+    else if ( evens == nullptr) {
+      return;
+    }
+    if ( evens->value % 2 == 1 ) {
       evens = nullptr;
-  } 
-  else if ( odds->value % 2 == 0) {
+    } 
+    else if ( odds->value % 2 == 0 ) {
       odds = nullptr;
+    }
   }
-  in = nullptr;
+  
+  //in = nullptr;
 }
 /* If you needed a helper function, write it here */
 
--- split_test.cpp
+++ split_test.cpp
@@ -4,25 +4,58 @@ using namespace std;
 
 int main(int argc, char* argv[]){
   //test1
-  Node *a = new Node(4, nullptr);
-  Node *b = new Node(7, a);
-  Node *c = new Node(9, b);
-  Node *d = new Node(6, c);
-  Node *e = new Node(154, d);
-  Node *f = new Node(155, e);
+  // Node *a = new Node(4, nullptr);
+  // Node *b = new Node(7, a);
+  // Node *c = new Node(9, b);
+  // Node *d = new Node(6, c);
+  // Node *e = new Node(154, d);
+  // Node *f = new Node(155, e);
 
-  //test2
-  Node *b = nullptr;
+  //test2 no value
+  //Node *b = nullptr;
   
+  //test3 all even
+  // Node *a = new Node(4, nullptr);
+  // Node *b = new Node(6, a);
+  // Node *c = new Node(144, b);
+  // Node *d = new Node(120, c);
+  // Node *e = new Node(154, d);
+  // Node *f = new Node(134, e);
+
+  //test 4 all odd
+  // Node *a = new Node(5, nullptr);
+  // Node *b = new Node(7, a);
+  // Node *c = new Node(145, b);
+  // Node *d = new Node(121, c);
+  // Node *e = new Node(155, d);
+  // Node *f = new Node(135, e);
+
+  //test5 negative numbers
+  // Node *a = new Node(4, nullptr);
+  // Node *b = new Node(6, a);
+  // Node *c = new Node(-144, b);
+  // Node *d = new Node(120, c);
+  // Node *e = new Node(154, d);
+  // Node *f = new Node(134, e);
+
+  //test6 zero
+  // Node *a = new Node(4, nullptr);
+  // Node *b = new Node(6, a);
+  // Node *c = new Node(0, b);
+  // Node *d = new Node(120, c);
+  // Node *e = new Node(154, d);
+  // Node *f = new Node(134, e);
 
   Node *odds = nullptr;
   Node *evens = nullptr;
-  //for test1
-  //split( f,  odds,  evens);
+
   
   //for test2
+  //split(b, odds, evens);
+
+  //for test #1, 3, 4, 5, 6
+  split(f, odds, evens);
   
-  split(b, odds, evens);
   while ( odds != nullptr) {
     cout << odds->value << endl;
     odds = odds->next;
@@ -32,15 +65,17 @@ int main(int argc, char* argv[]){
     cout << evens->value << endl;
     evens = evens->next;
   }
-  //for test1
-  // delete a;
-  // delete b;
-  // delete c;
-  // delete d;
-  // delete e;
-  // delete f;
+  //for test #1,3,4,5,6
+  delete a;
+  delete b;
+  delete c;
+  delete d;
+  delete e;
+  delete f;
   
   //for test2
-  delete b;
+  //delete b;
+
+  
   return 0;
 }
